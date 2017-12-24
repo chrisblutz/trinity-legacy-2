db33572d39df4d9aa73cd059903015bc69a0bd89
Christopher Lutz <lutzblox@gmail.com>
2017-12-23 20:44:34 -0500

    Fix Object.== trying to use a non-existent method

--- src/main/java/com/github/chrisblutz/trinity/lang/natives/NativeObject.java
+++ src/main/java/com/github/chrisblutz/trinity/lang/natives/NativeObject.java
@@ -26,6 +26,7 @@ public class NativeObject {
         TrinityNatives.registerMethod(CLASS, "<<", NativeUtilities.getDefaultUOEProcedure("<<"));
         TrinityNatives.registerMethod(CLASS, ">>", NativeUtilities.getDefaultUOEProcedure(">>"));
         TrinityNatives.registerMethod(CLASS, ">>>", NativeUtilities.getDefaultUOEProcedure(">>>"));
+        TrinityNatives.registerMethod(CLASS, "==", (runtime, thisObj, args) -> TyBoolean.valueFor(thisObj == runtime.getVariable("a")));
         TrinityNatives.registerMethod(CLASS, "<=>", NativeUtilities.getDefaultUOEProcedure("<=>"));
         TrinityNatives.registerMethod(CLASS, "getClass", (runtime, thisObj, args) -> NativeStorage.getClassObject(thisObj.getObjectClass()));
         TrinityNatives.registerMethod(CLASS, "isInstance", (runtime, thisObj, args) -> {
